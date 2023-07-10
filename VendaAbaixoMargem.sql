d3a1226f4b18717d36060b609bd6cf36f659c186
Álvaro Felipe <alvaroflp@outlook.com>
2023-07-10 09:53:27 -0300

    8156
    Vendas abaixo da margem

--- VendaAbaixoMargem.sql
+++ VendaAbaixoMargem.sql
@@ -3,16 +3,14 @@ Autor: Álvaro Felipe
 Descrição: Venda abaixo da margem
 Setor: Financeiro
 SQL para somar: round(sum(PCPEDI.pvenda - PCPEDI.VLCUSTOFIN * PCPEDI.QT),2)*-1 VALOR   
-informativo do relatório:
 
+informativo do relatório:
 var Msg: String;
 begin
-  Msg := Msg  
-   + 'Este ponto de auditoria tem como finalidade exibir as vendas abaixo da margem, exporte o relatório em excel';
+  Msg := Msg + 'Este ponto de auditoria tem como finalidade exibir as vendas abaixo da margem, para mais informações exporte o relatório em excel';
  ShowMessage(msg);
  end;
 */
-
 
 SELECT  *
             FROM PCPEDC, PCPEDI, PCCLIENT, 
@@ -27,7 +25,5 @@ SELECT  *
              AND PCPRACA.NUMREGIAO = PCTABPR.NUMREGIAO
              AND PCPEDC.POSICAO IN ('F', 'L', 'M', 'P', 'B')
              AND PCPEDC.DATA>=TRUNC(SYSDATE)-30
-           --  AND ((PCPEDC.CODFILIAL IN ([FILIAL])) OR ('99' IN ([FILIAL])))
-          ---   AND ((PCPRODUT.CODEPTO IN ([DEPARTAMENTO])) OR ( '9999' IN ([DEPARTAMENTO])))
              and PCPEDI.PVENDA < PCPEDI.VLCUSTOFIN                    
              AND PCPEDC.CONDVENDA NOT IN (4, 5, 6, 8, 10, 13,11)
